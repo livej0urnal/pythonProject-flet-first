529325fdeb31dc7bbed8ceddaf5648caeaf2da06
Igor Filippov <anubis3009@gmail.com>
2024-10-25 19:39:24 +0300

    #instal structure of projects install libraries for routing login and sign routes add styles to project

--- utils/style.py
+++ utils/style.py
@@ -2,3 +2,10 @@
 defaultBgColor = '#02091d'
 defaultFontColor = '#ffffff'
 defaultFontSize = '16'
+secondaryFontColor = '#9b9c9c'
+secondaryBgColor = '#1212222'
+inputBgColor = '#1a1a2a'
+hoverBgColor = '#0068f7'
+
+defaultWidthWindow = '1000'
+defaultHeightWindow = '600'
