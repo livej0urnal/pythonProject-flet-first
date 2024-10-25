5f7be7dab139e2c18abdba48de744ed2d9fe88e2
Igor Filippov <anubis3009@gmail.com>
2024-10-25 19:30:22 +0300

    #instal structure of projects install libraries for routing login and sign routes

--- router.py
+++ router.py
@@ -1,2 +1,16 @@
 import flet as ft
 from flet_route import Routing, path
+
+class Router:
+    def __init__(self, page: ft.Page):
+        self.page = page
+        self.app_routes = [
+            path(url='/', clear=True, view=LoginPage().view),
+            path(url='/signup', clear=False, view=SignupPage().view),
+        ]
+
+        Routing(
+            page=self.page,
+            app_routes=self.app_routes,
+        )
+        self.page.go(self.page.route)
