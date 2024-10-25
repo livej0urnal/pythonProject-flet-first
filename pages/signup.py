ec3dd5c47bf754f948cf74c9f1bb3f31d4020e1b
Igor Filippov <anubis3009@gmail.com>
2024-10-25 19:32:13 +0300

    #instal structure of projects install libraries for routing login and sign routes

---
+++ pages/signup.py
@@ -0,0 +1,14 @@
+import flet as ft
+from flet_route import Params, Basket
+
+
+class LoginPage:
+    def view(self, page: ft.Page, params: Params, basket: Basket):
+        page.title = 'Login Page'
+
+        return ft.View(
+            "/",
+            controls=[
+                ft.text('Login')
+            ]
+        )
