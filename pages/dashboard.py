6cc5f6db5f106fe756a8b82619a55faaf95acf30
Igor Filippov <anubis3009@gmail.com>
2024-10-26 17:33:43 +0300

    add method in dashboard input function sidebar menu

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -44,7 +44,7 @@ class DashboardPage:
             content=ft.Column(
                 controls=[
                     ft.Text('Navigation', color=menuFontColor, font_family='gotham', size=12),
-                    ft.TextButton('Home', icon='space_dashboard_rounded',style=style_menu),
+                    ft.TextButton('Home', icon='space_dashboard_rounded', style=style_menu),
                     ft.TextButton('Post', icon='post_add',style=style_menu),
                     ft.TextButton('Test', icon='verified_user',style=style_menu),
                 ]
