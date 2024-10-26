a6ecc7b6ea353fb4f594cf0de9a7053b77526217
Igor Filippov <anubis3009@gmail.com>
2024-10-26 15:00:08 +0300

    start SignupPage signup Form Data add signup inputs add validation class

--- utils/Validation.py
+++ utils/Validation.py
@@ -3,7 +3,7 @@ import re
 
 class Validation:
     def is_valid_email(self, email):
-        pattern = '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
         return re.match(pattern, email) is not None
 
     def is_valid_password(self, password):
@@ -11,7 +11,7 @@ class Validation:
             return False
         if not any(c.isdigit() for c in password):
             return False
-        if not re.search("[@_!#$%^&*()<>/\|}{~:]", password):
+        if not re.search(r"[@_!#$%^&*()<>/\|}{~:]", password):
             return False
 
         return True
