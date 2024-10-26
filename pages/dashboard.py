8227daae7d41244c8bf7821dea18698eca3c3d01
Igor Filippov <anubis3009@gmail.com>
2024-10-26 17:57:26 +0300

    add method in dashboard input function sidebar menu header

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -59,7 +59,7 @@ class DashboardPage:
                 ft.Row(
                     controls=[
                         ft.CircleAvatar(
-                            foreground_image_src='images/avatar.png',
+                            foreground_image_src='https://core.telegram.org/file/811140327/1/zlN4goPTupk/9ff2f2f01c4bd1b013',
                             content=ft.Text('A')
                         ),
                         ft.IconButton(
