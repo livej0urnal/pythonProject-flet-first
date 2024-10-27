b4be56ad228dce888de41ebbd4fa8288539a6232
Igor Filippov <anubis3009@gmail.com>
2024-10-27 18:45:19 +0200

    add posting page fix menu add inputs fields add telegram ip file

--- utils/request.py
+++ utils/request.py
@@ -7,4 +7,11 @@ def getUpdate(token):
     result = requests.get(f'{url}{token}/getUpdates').json()
     return print(result)
 
-getUpdate('8159762497:AAFpOwtJcTQ3-00APg4AfabhRB9pF77CJKE')
+# getUpdate('8159762497:AAFpOwtJcTQ3-00APg4AfabhRB9pF77CJKE')
+
+def sendMessage(token, channel, text):
+    try:
+        return requests.post(
+            url=f'{url}{token}/sendMessage',
+            data={'chat_id': channel, 'text': text}
+        )
