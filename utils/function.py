d8faa5002d56bfa53befdcbf27aec7d6d4953aea
Igor Filippov <anubis3009@gmail.com>
2024-10-27 20:19:00 +0200

    add posting page fix menu add inputs fields add telegram ip file complete debugging file load in post add upload dir

--- utils/function.py
+++ utils/function.py
@@ -1,5 +1,14 @@
 import hashlib
+import string
+import random
+
 
 #method create password hash
 def hash_password(password):
     return hashlib.md5(password.encode('utf-8')).hexdigest()
+
+# generate random name for file
+def p_link_generate(length):
+    letters_and_digits = string.ascii_letters + string.digits
+    result_str = ''.join(random.choice(letters_and_digits) for i in range(length))
+    return result_str
