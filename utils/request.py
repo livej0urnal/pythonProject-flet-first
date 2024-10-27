99fbba8042bdfa87037f616f11ad3a06dbf612eb
Igor Filippov <anubis3009@gmail.com>
2024-10-27 18:42:19 +0200

    add posting page fix menu add inputs fields add telegram ip file

--- utils/request.py
+++ utils/request.py
@@ -2,6 +2,7 @@ import requests
 
 url = 'https://api.telegram.org/bot'
 
+#function for get updates from api telegram
 def getUpdate(token):
     result = requests.get(f'{url}{token}/getUpdates').json()
     return print(result)
