22500781a8bb3002cd259005e9a68cefe11d7ec8
Igor Filippov <anubis3009@gmail.com>
2024-11-10 19:15:40 +0200

    #add new delay post table #add new method insert and get posts

--- utils/Validation.py
+++ utils/Validation.py
@@ -1,11 +1,15 @@
 import re
 
+from repath import pattern
+
 
 class Validation:
+    # method check validation email
     def is_valid_email(self, email):
         pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
         return re.match(pattern, email) is not None
 
+    # check password valid
     def is_valid_password(self, password):
         if len(password) < 5:
             return False
@@ -15,3 +19,8 @@ class Validation:
             return False
 
         return True
+
+    # check time format
+    def is_valid_date(self, date):
+        pattern = r'^\d{2}:\d{2}$'
+        return re.match(pattern, date) is not None
